31e7794ff1c9b23530680d1a862ec430cc0fcab4
Yeon Jeffrey <yeonjeseo@hotmail.com>
2022-04-06 23:05:19 +0900

    fix: config s3 객체 구조 변경

--- server/config/config.js
+++ server/config/config.js
@@ -1,14 +1,19 @@
 import dotenv from "dotenv";
-dotenv.config();
+
+dotenv.config({
+  path: `${process.cwd().split("/").slice(0, -1).join("/")}/.env`,
+});
 
 const envVars = process.env;
 
 export default {
   NODE_ENV: envVars.NODE_ENV,
   PORT: envVars.PORT,
-  s3Config: {
-    // S3_BUCKET_NAME : ,
-    // S3_ACCESS_KEY: ,
-    // S3_SECRET_KEY:
+  s3: {
+    S3_BUCKET_NAME: envVars.S3_BUCKET_NAME,
+    s3Config: {
+      accessKeyId: envVars.S3_ACCESS_KEY,
+      secretAccessKey: envVars.S3_SECRET_KEY,
+    },
   },
 };
