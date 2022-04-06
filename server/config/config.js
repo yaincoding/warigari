f5412c3ed7eed121739218c60b2dbb63ab1b2146
Yeon Jeffrey <yeonjeseo@hotmail.com>
2022-04-06 21:29:10 +0900

    feat: S3 작업 뼈대 준비

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,4 @@
 import dotenv from "dotenv";
-
 dotenv.config();
 
 const envVars = process.env;
@@ -7,4 +6,9 @@ const envVars = process.env;
 export default {
   NODE_ENV: envVars.NODE_ENV,
   PORT: envVars.PORT,
+  s3Config: {
+    // S3_BUCKET_NAME : ,
+    // S3_ACCESS_KEY: ,
+    // S3_SECRET_KEY:
+  },
 };
