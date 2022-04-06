b9ee97bea5d5f432a3a954f81a543bbf1fe760d8
yaincoding <yaincoding@gmail.com>
2022-04-06 21:52:55 +0900

    무신사 스크래핑 스크립트

--- scripts/musinsa_scrape.py
+++ scripts/musinsa_scrape.py
@@ -1,40 +1,72 @@
+import csv
+from genericpath import exists
+import re
+import os
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
-import re
 import argparse
+import time
+import pandas as pd
 
-category_map = {
-    "상의": "001",
-    "아우터": "002",
-    "바지": "003",
-    "원피스": "020"
+categoryMap = {
+    "001": "top",
+    "002": "outer",
+    "003": "bottom",
+    "020": "onepiece",
+    "022": "skirt"
 }
 
-category = "001"
-page = 1
 
-
-def createItems(category, page):
-    items = []
+def parseItems(categoryNumber, page):
+    parsed_items = []
     html = urlopen(
-        f"https://www.musinsa.com/category/001?d_cat_cd={category}&brand=&rate=&page_kind=search&list_kind=small&sort=pop&sub_sort=&page={page}&display_cnt=90&sale_goods=&group_sale=&kids=N&ex_soldout=&color=&price1=&price2=&exclusive_yn=&shoeSizeOption=&tags=&campaign_id=&timesale_yn=&q=&includeKeywords=&measure=")
+        f"https://www.musinsa.com/category/001?d_cat_cd={categoryNumber}&brand=&rate=&page_kind=search&list_kind=small&sort=pop&sub_sort=&page={page}&display_cnt=90&sale_goods=&group_sale=&kids=N&ex_soldout=&color=&price1=&price2=&exclusive_yn=&shoeSizeOption=&tags=&campaign_id=&timesale_yn=&q=&includeKeywords=&measure=")
     bsObject = BeautifulSoup(html, "html.parser")
-    item_list = bsObject.find('div', attrs={
+    items = bsObject.find('div', attrs={
         'class': 'list-box box'}).findAll('li', attrs={'class': 'li_box'})
-    for i in item_list:
+
+    df = pd.DataFrame(columns=['name', 'category', 'img_url', 'price'])
+    for i in items:
         img_url = i.find('img')[
             'data-original'].replace('_125.jpg', '_500.jpg')
         name = i.find('a', attrs={'name': 'goods_link'})['title']
         price = i.find('p', attrs={'class': 'price'}).decode_contents()
         price = re.sub(r'\<del\>.*\<\/del\>', '', price).strip()
-        price = int(re.sub(r'[원,]', '', price))
-    items.append({
-        'name': name,
-        'category': category,
-        'img_url': img_url,
-        'price': price,
-    })
+        parsed_items.append({
+
+            'name': name,
+            'category': categoryMap[categoryNumber],
+            'img_url': img_url,
+            'price': price,
+        })
+    df = pd.DataFrame(data=parsed_items, columns=[
+                      'name', 'category', 'img_url', 'price'])
+    return df
+
+
+def scrape(save_dir, pages_per_category):
+    df = pd.DataFrame(columns=['name', 'category', 'img_url', 'price'])
+    for (categoryNumber, categoryName) in categoryMap.items():
+        category_dir = f'{save_dir}/{categoryName}'
+        if not os.path.isdir(category_dir):
+            os.makedirs(category_dir, exist_ok=True)
+        for page in range(1, pages_per_category + 1):
+            fname = f'{page}.csv'
+            if os.path.isfile(f'{category_dir}/{fname}'):
+                continue
+            df = parseItems(categoryNumber, page)
+            df.to_csv(f'{category_dir}/{fname}', sep=',', header=True,
+                      index=False, quoting=csv.QUOTE_ALL)
+            time.sleep(1)
+
+    return df
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("--save_dir")
+    parser.add_argument("--pages_per_category")
+    args = parser.parse_args()
+    save_dir = args.save_dir
+    pages_per_category = int(args.pages_per_category)
+    scrape(save_dir, pages_per_category)
