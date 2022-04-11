fe0ceba3c3ea8ecbba171351a9d4a20ac30056bd
yaincoding <yaincoding@gmail.com>
2022-04-12 00:50:50 +0900

    1. elasticsearch, mysql host name 수정(docker network name=warigari_net) 2. feature None 아닐 때에만 tolist() 호출하여 python array로 변경하도록 수정 3. fashion index alias 자동 삭제/등록 코드 작성 4. 문서 bulk index 코드 작성

--- scripts/data/elasticsearch/index_data.py
+++ scripts/data/elasticsearch/index_data.py
@@ -2,25 +2,26 @@ from datetime import datetime
 import os
 import elasticsearch
 import pymysql
-from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch, helpers
+from tqdm import tqdm
 
 from PIL import Image
 import io
 import urllib.request
-from scripts.data.elasticsearch.image_feature import FashionDetector, FeatureExtractor
-
+from image_feature import FashionDetector, FeatureExtractor
 
 # mysql 연결
-mysql_conn = pymysql.connect(host='localhost', user='root',
-                             password=os.environ.get('MYSQL_ROOT_PASSWORD'), charset='utf8')
+mysql_conn = pymysql.connect(host='warigari_mysql', user='root', password=os.environ.get(
+    'MYSQL_ROOT_PASSWORD'), charset='utf8')
 cursor = mysql_conn.cursor()
 
 # elasticsearch 연결
 es = Elasticsearch(
-    hosts='http://localhost:9200'
+    hosts='http://warigari_elasticsearch:9200'
 )
 
 fashion_alias = 'fashion'
+
 
 def create_index():
     now = datetime.now().strftime('%Y%m%d%H%M%S')
@@ -44,23 +45,23 @@ def create_index():
                     "discard_punctuation": "true",
                     "user_dictionary": "user_dictionary.txt"
                 }
-            }, 
+            },
             "filter": {
                 "synonym_filter": {
                     "type": "synonym_graph",
                     "synonyms_path": "synonyms.txt"
                 }
-            }, 
+            },
             "analyzer": {
                 "index_analyzer": {
                     "type": "custom",
                     "char_filter": [
-                        "clean_filter"  
+                        "clean_filter"
                     ],
                     "tokenizer": "kor_tokenizer",
                     "filter": [
                         "lowercase",
-                        "nori_readingform"  
+                        "nori_readingform"
                     ]
                 },
                 "search_analyzer": {
@@ -102,60 +103,87 @@ def create_index():
                 "type": "dense_vector",
                 "dims": 256
             }
-        } 
+        }
     }
 
-    result = es.indices.create(index=index_name, settings=settings, mappings=mappings)
+    result = es.indices.create(
+        index=index_name, settings=settings, mappings=mappings)
     if result['acknowledged']:
         print(f'index {index_name}이 생성되었습니다.')
         return index_name
 
-def update_alias():
+
+def update_alias(index_name):
     try:
         aliases = es.indices.get_alias(name=fashion_alias)
+        es.indices.put_alias(name=fashion_alias, index=index_name)
         if aliases:
             for index in aliases:
                 es.indices.delete_alias(index=index, name=fashion_alias)
     except elasticsearch.NotFoundError:
-        aliases = None
+        es.indices.put_alias(name=fashion_alias, index=index_name)
 
-    es.indices.put_alias(name=fashion_alias, index=index_name) 
-    
 
 index_name = create_index()
+update_alias(index_name)
 
 detector_model_path = "/tf_models/object_detection"
 detector = FashionDetector(detector_model_path)
 classifier_model_path = "/tf_models/classifier"
 feature_extractor = FeatureExtractor(classifier_model_path)
 
+
 def load_image(image_url):
-        res = urllib.request.urlopen(image_url).read()
-        img = Image.open(io.BytesIO(res))
-        return img
-    
+    res = urllib.request.urlopen(image_url).read()
+    img = Image.open(io.BytesIO(res))
+    return img
+
+
 def get_vectors(image_url):
     img = load_image(image_url)
     crops = detector.crop(img, min_score=0.6)
-    crop_feature_vector = None
     if len(crops) > 0:
-        crop_feature_vector = feature_extractor.extract_from_tensor(crops[0]).tolist()
-    full_feature_vector = feature_extractor.extract_from_image(img).tolist()
+        crop_feature_vector = feature_extractor.extract_from_tensor(
+            crops[0])
+        if crop_feature_vector is not None:
+            crop_feature_vector = crop_feature_vector.tolist()
+    full_feature_vector = feature_extractor.extract_from_image(img)
+    if full_feature_vector is not None:
+        full_feature_vector = full_feature_vector.tolist()
 
     return {
         'crop_feature_vector': crop_feature_vector,
         'full_feature_vector': full_feature_vector
     }
 
+
 def index_data():
     cursor.execute('USE warigari;')
-    cursor.execute('SELECT itemId, productName, productPart, unitPrice, imageUrl FROM Items;')
+    cursor.execute(
+        'SELECT itemId, productName, productPart, unitPrice, imageUrl FROM Items;')
     result = cursor.fetchall()
-    for row in result:
+    docs = []
+    for row in tqdm(result):
         itemId, productName, productPart, unitPrice, imageUrl = row
         vectors = get_vectors(imageUrl)
         crop_feature_vector = vectors['crop_feature_vector']
         full_feature_vector = vectors['full_feature_vector']
-        print(crop_feature_vector)
-        print(full_feature_vector)
-        break
+
+        doc = {
+            '_index': fashion_alias,
+            '_source': {
+                'id': itemId,
+                'productName': productName,
+                'productPart': productPart,
+                'unitPrice': unitPrice,
+                'crop_feature_vector': crop_feature_vector,
+                'full_feature_vector': full_feature_vector
+            }
+        }
+        docs.append(doc)
+        if len(docs) >= 100:
+            helpers.bulk(es, docs)
+            docs = []
+
+
+index_data()
