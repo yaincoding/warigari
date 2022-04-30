531424e55e3fe7266e92acd0ed64fbdbb8677cbf
yaincoding <yaincoding@gmail.com>
2022-05-01 03:13:19 +0900

    1. search router 추가 2. init.js 에서 app 실행 코드 이동

--- server/server.js
+++ server/server.js
@@ -1,10 +1,18 @@
 import express from "express";
 import globalRouter from "./routes/global.routes.js";
+import searchRouter from "./routes/search/search.js";
+
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.set(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use("/", globalRouter);
+app.use("/api/search", searchRouter);
+
+app.listen(PORT, () => {
+  console.log(`express server is listening on ${PORT}`);
+});
 
 export default app;
