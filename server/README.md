39f9d9d429970adb322f3cae67d271d92b49a64e
Yeon Jeffrey <yeonjeseo@hotmail.com>
2022-05-12 18:55:51 +0900

    fix: GraphQL 세팅

--- server/README.md
+++ server/README.md
@@ -3,3 +3,14 @@
 1. 모듈 설치
 
 - `npm i --save express-graphql graphql`
+
+## express 서버 세팅
+
+1. 모듈 설치
+
+- `cd ~/server/`
+- `npm i`
+
+2. npm 스크립트 실행
+
+- `npm run dev`
--- server/server.js
+++ server/server.js
@@ -1,10 +1,24 @@
 import express from "express";
 import globalRouter from "./routes/global.routes.js";
+
+// 2022.05.12
+import { graphqlHTTP } from "express-graphql";
+import { buildSchema } from "graphql";
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = { hello: () => "Hello World!" };
+
 const app = express();
 
 app.set(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use("/graphql", graphqlHTTP({ schema, rootValue: root, graphiql: true }));
 app.use("/", globalRouter);
 
 export default app;
