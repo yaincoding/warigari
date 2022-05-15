6923f550b8765c002cea30d48206e99000171e03
Yeon Jeffrey <yeonjeseo@hotmail.com>
2022-05-16 00:04:04 +0900

    fix: non-scalar 타입 추가 (Date)

--- server/graphql/index.js
+++ server/graphql/index.js
@@ -1,28 +1,54 @@
-import Graphql from "graphql";
-import { readOneUser } from "../services/user.js";
+import Graphql, { GraphQLScalarType } from "graphql";
+import { readOneUser, readAllusers } from "../services/user.js";
 
 const { buildSchema } = Graphql;
 
+// 도대체 어떻게 작동하는거지?
+new GraphQLScalarType({
+  name: "Date",
+  description: "Date custom scalar type",
+  serialize(value) {
+    return value.getTime(); // Convert outgoing Date to integer for JSON
+  },
+  parseValue(value) {
+    return new Date(value); // Convert incoming integer to Date
+  },
+  parseLiteral(ast) {
+    if (ast.kind === Kind.INT) {
+      return new Date(parseInt(ast.value, 10)); // Convert hard-coded AST string to integer and then to Date
+    }
+    return null; // Invalid hard-coded value (not an integer)
+  },
+});
+
 /**
  * exclamation mark : non null
+ * scalar type : built-in graphql type (String, Int, Boolean, ID ....)
  */
 const schema = buildSchema(`
   type Query {
     user(account: String!): User
+    users: [User]
   }
 
   type User {
+    id: ID
     account: String
     name: String
+    createdAt: Date!
   }
+
+  scalar Date
 `);
 
 const resolver = {
+  // 상단에서 non-scalar 추가
   user: async (args, context, info) => {
     const { account } = args;
     const user = await readOneUser(account);
     return user;
   },
+  users: async () => await readAllusers(),
 };
 
 export { schema, resolver };
--- server/services/user.js
+++ server/services/user.js
@@ -2,3 +2,5 @@ import db from "../models/index.js";
 
 export const readOneUser = (account) =>
   db.Users.findOne({ where: { account } });
+
+export const readAllusers = () => db.Users.findAll();
